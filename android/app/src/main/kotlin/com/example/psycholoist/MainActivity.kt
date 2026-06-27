f69359ee5bf0da4f3cbc0da6b304504018a28506
sanjanabhattiya7 <159808966+sanjanabhattiya7@users.noreply.github.com>
2026-06-27 15:47:09 +0530

    Initial Flutter project setup

---
+++ android/app/src/main/kotlin/com/example/psycholoist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.psycholoist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/feature/splash/screen/splash_screen.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+import 'package:psycholoist/feature/onboarding/screen/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+       Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingScreen(),),
+        );
+    }
+    
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/logo.png",
+              height:200,
+              width: 250),
+
+              Text("Wellhope Hiring",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF2D5356),
+                fontFamily: "AbrilFatface",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
